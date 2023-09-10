2b628b4ff42caf1fa48971045b59f7e592b92974
Aditya Dhanwai <aditya.dhanwai@mitaoe.ac.in>
2023-09-10 10:10:46 +0530

    third commit

--- hello.java
+++ hello.java
@@ -1,7 +1,8 @@
-class a
+class hello
 {
     public static void main(String ar[])
     {
         System.out.println("Hello from Aditya Dhanwai!!");
+        System.out.println("Hello from Git ");
     }
 }
